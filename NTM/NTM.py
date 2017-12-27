7338e3e1984b913f930d1d7020c4bd7ac6ccbaad
philippe debie <philippe.mmd@gmail.com>
2017-12-27 13:18:17 +0100

    Version 1?

--- NTM/NTM.py
+++ NTM/NTM.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import pandas as pd
 import numpy as np
+import NTMCell
 
 def makeNTM(x):
     steps = x.get_shape()[1]
@@ -11,6 +12,17 @@ def makeNTM(x):
     M = makeStartState([8, 4])
     wRead = makeStartState([8])
     wWrite = makeStartState([8])
+
+    ntmCell = NTMCell("ntm")
+
+    output = []
+
+    for i in range(0:steps):
+        input = tf.slice(x,[0,i],[x.get_shape()[0],1])
+        prevLSTMOutput, prevLSTMState, O, prevRead, M, wRead, wWrite = ntmCell.buildTimeLayer(input, prevLSTMOutput, prevLSTMState, prevRead, M, wRead, wWrite)
+        output.append(O)
+
+    result = tf.concat(output, 1)
 
 M = tf.Variable(tf.random_normal([8,4]))
 O = tf.Variable(tf.random_normal([20]))
